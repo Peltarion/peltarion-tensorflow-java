cb82ee4c8fdb6a3ba0491daed2a03e932a22694a
Samuel Otter <samuel@peltarion.com>
2020-08-05 11:42:13 +0200

    Try forcing -fPIC

--- tensorflow-core/tensorflow-core-api/build.sh
+++ tensorflow-core/tensorflow-core-api/build.sh
@@ -13,7 +13,7 @@ if [[ -d $BAZEL_VC ]]; then
     export PATH=$PATH:$(pwd)/bazel-tensorflow-core-api/external/mkl_windows/lib/
     export PYTHON_BIN_PATH=$(which python.exe)
 else
-    export BUILD_FLAGS="--copt=-msse4.1 --copt=-msse4.2 --copt=-mavx `#--copt=-mavx2 --copt=-mfma` --cxxopt=-std=c++14 --host_cxxopt=-std=c++14 --linkopt=-lstdc++ --host_linkopt=-lstdc++"
+    export BUILD_FLAGS="--copt=-msse4.1 --copt=-msse4.2 --copt=-mavx `#--copt=-mavx2 --copt=-mfma` --cxxopt=-std=c++14 --host_cxxopt=-std=c++14 --linkopt=-lstdc++ --host_linkopt=-lstdc++ --copt=-g --copt=-fPIC"
     export PYTHON_BIN_PATH=$(which python3)
 fi
 
